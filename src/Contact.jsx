5590fbc51106ce193710b709bae47e2107f1a087
Shadajifrin <fathimashadanp@gmail.com>
2024-03-19 04:43:51 +0530

    firebase done for form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,37 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 import Back from './components/Back'
+
 
 const Contact = () => {
     const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2300764.4912408697!2d76.42135263455393!3d9.970570143082359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1710235938887!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }; const res = await fetch('https://react-contact-8145f-default-rtdb.firebaseio.com/FormData.json', options);
+        if (res.ok) {
+            alert('Message sent Successfully');
+        } else {
+            alert('Error Occurred');
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
     return (
         <div>
             <Back title='Contact Us' />
@@ -29,14 +57,13 @@ const Contact = () => {
                                 <p> + 1235 2355 98</p>
                             </div>
                         </div>
-                        <form action=''>
+                        <form onSubmit={handleSubmit}>
                             <div className="flexSB">
-                                <input type='text' placeholder='Name' />
-                                <input type='email' placeholder='Email' />
+                                <input type='text' name='name' placeholder='Name' value={formData.name} onChange={handleChange} required />
+                                <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} required />
                             </div>
-                            <input type='text' placeholder='Subject' />
-                            <textarea cols='30' rows='10'>
-                                Create a message here...
+                            <input type='text' placeholder='Subject' name='subject' value={formData.subject} onChange={handleChange} required />
+                            <textarea cols='30' rows='10' name='message' value={formData.message} onChange={handleChange} placeholder='Create a message here...' required>
                             </textarea>
                             <button className='primary-btn'>SEND MESSAGE</button>
                         </form>
--- src/components/EnrollmentForm.jsx
+++ src/components/EnrollmentForm.jsx
@@ -1,74 +1,97 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
+import Back from './Back';
 
 const EnrollmentForm = () => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [selectedCourse, setSelectedCourse] = useState('');
-  
- 
+  const [user, setUser] = useState({
+    Name: '',
+    Email: '',
+    Number: '',
+    Course: ''
+  });
+
   const courses = [
     { id: 1, name: 'Introducing to Software Engineering' },
     { id: 2, name: 'Enhancing Adobe Photoshop CC 2020 Skills' },
     { id: 3, name: 'HTML, CSS, and Javascript for Web Developers' },
-    { id: 4,name:'Introducing to Programming with WordPress'},
-    { id: 5,name:'Introducing to Programming with ReactJS'},
-    { id: 6,name:'Learn Frontend Programming Language'}, 
-    { id: 7,name:'Introducing to Programming with HTML / CSS'}, 
-    { id: 8,name:'Introducing to Programming with JAVA'},
+    { id: 4, name: 'Introducing to Programming with WordPress' },
+    { id: 5, name: 'Introducing to Programming with ReactJS' },
+    { id: 6, name: 'Learn Frontend Programming Language' },
+    { id: 7, name: 'Introducing to Programming with HTML / CSS' },
+    { id: 8, name: 'Introducing to Programming with JAVA' },
   ];
 
-  
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log({ fullName, email, phoneNumber, selectedCourse });
-    setFullName('');
-    setEmail('');
-    setPhoneNumber('');
-    setSelectedCourse('');
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    };
+
+    const res = await fetch('https://reactfirebase-ed6c9-default-rtdb.firebaseio.com/UserData.json', options);
+    if (res.ok) {
+      alert('Enrolled Successfully');
+    } else {
+      alert('Error Occurred');
+    }
   };
 
   return (
-    <div>
+    <div className='enrollment-form'>
+      <Back title="Enrollment Form" />
       <h2>Enrollment Form</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="fullName">Full Name:</label>
+          <label className="fullName">Full Name:</label>
           <input
             type="text"
             id="fullName"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
+            name="Name"
+            value={user.Name}
+            autoComplete='off'
+            onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label className="email">Email:</label>
           <input
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="Email"
+            value={user.Email}
+            autoComplete='off'
+            onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="phoneNumber">Phone Number:</label>
+          <label className="phoneNumber">Phone Number:</label>
           <input
             type="tel"
             id="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            name="Number"
+            value={user.Number}
+            autoComplete='off'
+            onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="selectedCourse">Select Course:</label>
+          <label className="selectedCourse">Select Course:</label>
           <select
             id="selectedCourse"
-            value={selectedCourse}
-            onChange={(e) => setSelectedCourse(e.target.value)}
+            name="Course"
+            value={user.Course}
+            onChange={handleChange}
             required
           >
             <option value="">Select a course</option>
